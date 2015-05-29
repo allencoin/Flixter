5ddf672dc429e3a66efc9096246da73bfc4a1878
Allen Coin <allencoin@gmail.com>
2015-05-29 09:13:14 +0000

    some basic styling on the course show page

---
+++ test/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :course do
+    title "How To Write a Test"
+    description "Writing a test 101."
+    cost "404"
+  end
+end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "new_course" do
+	  course = FactoryGirl.create(:course)
+	  assert_response :success
+	end
+
+	test "course show page, not found" do
+		get :course, :course_id => 'blerg'
+		assert_response :not_found
+	end
 end
