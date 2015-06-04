594ec111ca08412d4a7217cb51e9b7d633f9a618
Allen Coin <allencoin@gmail.com>
2015-06-04 18:33:56 +0000

    add new lesson form to a modal

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,12 +1,8 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, :only => [:new, :create]
+  before_action :require_authorized_for_current_section, :only => [:create]
   before_action :require_authorized_for_current_lesson, :only => [:update]
   skip_before_filter :verify_authenticity_token, :only => [:update]
-
-  def new
-    @lesson = Lesson.new
-  end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
